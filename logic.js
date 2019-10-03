d12bef680a5643792911da280411e83f1b3e3a15
Meero Harootunian <meero.harootunian@gmail.com>
2019-10-02 23:12:59 -0700

    complete code re-write

--- logic.js
+++ logic.js
@@ -1,252 +1,192 @@
-//TODOS:
-//Check if another player is joined in game, or when a player does join
-//check if player made a move and compare moves
-
-let rock = $("#submit-rock"),
-  paper = $("#submit-paper"),
-  scissors = $("#submit-scissors")
-
-let playerName,
-  nameInput = $("#name-input"),
-  nameSubmit = $("#submit-name")
-
-let game,
-  gameInput = $("#game-input"),
-  gameSubmit = $("#submit-game")
-
-let player
-
-let player1Choice = "!",
-  player2Choice = "!"
-
-function nameValidation(name) {
-  if (name === "" || name === null) {
-    alert("please fill out a name and try again")
-  } else {
-    playerName = name;
-    nameSubmit.prop("disabled", true)
-    nameInput.prop("disabled", true)
-    gameInput.prop("disabled", false)
-    gameSubmit.prop("disabled", false)
-    rock.prop("disabled", false)
-    paper.prop("disabled", false)
-    scissors.prop("disabled", false)
-  }
-}
-
-function startGame() {
-  player1Choice = "!"
-  player2Choice = "!"
-
-  db.ref().set({
-    
-  })
-}
-
-function whoWon() {
-  if (player1Choice === "rock" && player2Choice === "rock") {
-    resetGame()
-    return "tie"
-  }
-  if (player1Choice === "rock" && player2Choice === "paper") {
-    resetGame()
-    return "player2"
-  }
-  if (player1Choice === "rock" && player2Choice === "scissors") {
-    resetGame()
-    return "player1"
-  }
-
-  if (player1Choice === "paper" && player2Choice === "rock") {
-    resetGame()
-    return "player1"
-  }
-  if (player1Choice === "paper" && player2Choice === "paper") {
-    resetGame()
-    return "tie"
-  }
-  if (player1Choice === "paper" && player2Choice === "scissors") {
-    resetGame()
-    return "player2"
-  }
-
-  if (player1Choice === "scissors" && player2Choice === "rock") {
-    resetGame()
-    return "player2"
-  }
-  if (player1Choice === "scissors" && player2Choice === "paper") {
-    resetGame()
-    return "player1"
-  }
-  if (player1Choice === "scissors" && player2Choice === "scissors") {
-    resetGame()
-    return "tie"
-  }
-
-}
-
-function resetGame() {
-  db.ref().update({
-    player1Choice: "!",
-    player2Choice: "!"
-  })
-}
-
-function joinGame() {
-  db.ref().once("value", function (snapshot) {
-
-    if (snapshot.exists()) {
-      let data = snapshot.val()
-      console.log(data)
-      if (data.gameData.players === 1) {
-        player = 2
-        db.ref(`/gameData`).set({
-          players: 2
-        })
-      } else if (data.gameData.players >= 2) {
-        alert("nope, it dun broked")
-      } else {
-
-      }
-    } else {
-      console.log("PLAYER 1")
-      player = 1
-      db.ref(`/gameData`).set({
-        players: 1
-      })
-      alert("waiting for player 2")
-    }
-
-  })
-}
-
-rock.prop("disabled", true)
-paper.prop("disabled", true)
-scissors.prop("disabled", true)
-gameSubmit.prop("disabled", true)
-gameInput.prop("disabled", true)
-
-var firebaseConfig = {
-  apiKey: "AIzaSyAe5LFUQ4xcX82zYTlVqrHtkV9SBF2Lw0Q",
-  authDomain: "rawcpayperskiz0rz.firebaseapp.com",
-  databaseURL: "https://rawcpayperskiz0rz.firebaseio.com",
-  projectId: "rawcpayperskiz0rz",
-  storageBucket: "",
-  messagingSenderId: "856845378208",
-  appId: "1:856845378208:web:605d334a505f25bd839fc5"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-let db = firebase.database()
-
 $(function () {
+  let rock = $("#submit-rock"),
+      paper = $("#submit-paper"),
+      scissors = $("#submit-scissors")
 
-  db.ref("/player1Choice").on("value", function (snapshot) {
-    let data = snapshot.val()
-    console.log("Player1 choice: " + data)
-    if (data === "!") {
-      console.log("equals !")
-      //dont do anything
-    } else {
-      console.log(`player1Choice Response: ${data}`)
-      if (data === null) {
-        //alert(`player1 has not chosen`)
-      } else {
-        alert(`player1 has chosen: ${data}`)
-        player1Choice = data
-        console.log("p1 choice: " + player1Choice)
-        console.log("p1c:" + player1Choice + " && p2c: " + player2Choice)
+  let game,
+      gameSubmit = $("#submit-game")
 
-        if (player1Choice !== "!" && player2Choice !== "!") {
-          alert("both players have chosen!")
-          alert(whoWon())
-        }
+  let player
+
+  let player1Choice = "!",
+      player2Choice = "!"
+
+  let player1Wins = 0,
+      player1Losses = 0,
+      ties = 0
+
+  let player2Wins = 0,
+      player2Losses = 0
+
+  let inGame = false
+
+  var firebaseConfig = {
+      apiKey: "AIzaSyAe5LFUQ4xcX82zYTlVqrHtkV9SBF2Lw0Q",
+      authDomain: "rawcpayperskiz0rz.firebaseapp.com",
+      databaseURL: "https://rawcpayperskiz0rz.firebaseio.com",
+      projectId: "rawcpayperskiz0rz",
+      storageBucket: "",
+      messagingSenderId: "856845378208",
+      appId: "1:856845378208:web:605d334a505f25bd839fc5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  let db = firebase.database()
+
+  function joinGame() {
+      db.ref("gameData").once("value", function (snapshot) {
+          if (snapshot.exists()) { // if data exists...
+              console.log("data exists")
+              if (snapshot.val().players === 2) {
+                  console.log("game has enough players")
+              } else if (snapshot.val().players === 1) {
+                  db.ref("/gameData").update({
+                      players: 2
+                  })
+                  player = 2
+                  inGame = true
+              }
+          } else { // data does not exist
+              db.ref("/gameData").set({
+                  players: 1
+              })
+              player = 1
+              inGame = true
+          } // end if-block
+      })// end db-ref
+  }
+
+  window.addEventListener("unload", function (e) {
+      if (inGame) {
+          db.ref().set({})
       }
-    }
-  })
-
-  db.ref("/player2Choice").on("value", function (snapshot) {
-    let data = snapshot.val()
-    console.log(`player2Choice Response: ${data}`)
-    console.log("Player2 choice: " + data)
-
-    if (data === "!") {
-      console.log("equals !")
-
-      //dont do anything
-    } else {
-      if (data === null) {
-        //alert(`player2 has not chosen`)
-      } else {
-        alert(`player2 has chosen: ${data}`)
-        player2Choice = data
-        console.log("p2 choice: " + player2Choice)
-        console.log("p1c:" + player1Choice + " && p2c: " + player2Choice)
-        if (player1Choice !== "!" && player2Choice !== "!") {
-          alert("both players have chosen!")
-          alert(whoWon())
-        }
-      }
-    }
   })
 
   rock.click(function () {
-    if (player === 1) {
-      db.ref().update({
-        player1Choice: "rock"
-      })
-    } else {
-      db.ref().update({
-        player2Choice: "rock"
-      })
-    }
-
+      if (player === 1) {
+          db.ref().update({
+              player1Choice: "rock"
+          })
+      } else if (player === 2) {
+          db.ref().update({
+              player2Choice: "rock"
+          })
+      }
   })
   paper.click(function () {
-    if (player === 1) {
-      db.ref().update({
-        player1Choice: "paper"
-      })
-    } else {
-      db.ref().update({
-        player2Choice: "paper"
-      })
-    }
-
+      if (player === 1) {
+          db.ref().update({
+              player1Choice: "paper"
+          })
+      } else if (player === 2) {
+          db.ref().update({
+              player2Choice: "paper"
+          })
+      }
   })
   scissors.click(function () {
-    if (player === 1) {
-      db.ref().update({
-        player1Choice: "scissors"
-      })
-    } else {
-      db.ref().update({
-        player2Choice: "scissors"
-      })
-    }
-
+      if (player === 1) {
+          db.ref().update({
+              player1Choice: "scissors"
+          })
+      } else if (player === 2) {
+          db.ref().update({
+              player2Choice: "scissors"
+          })
+      }
   })
-
-  nameSubmit.click(function () {
-    name = nameInput.val()
-    nameValidation(name)
-  })
-
   gameSubmit.click(function () {
-    game = $("#game-input").val()
-    $(this).prop("disabled", true)
-    $("#game-input").prop("disabled", true)
-    joinGame()
+      joinGame();
   })
 
+  function endGame(player1Choice, player2Choice) {
+      if (player1Choice === "rock" && player2Choice === "rock") {
+          ties++
+      }
+      if (player1Choice === "rock" && player2Choice === "paper") {
+          player2Wins++
+          player1Losses++
+      }
+      if (player1Choice === "rock" && player2Choice === "scissors") {
+          player1Wins++
+          player2Losses++
+      }
 
-  //This code block triggers when the page closes, or changes
-  window.addEventListener("unload", function (e) {
-    //db.ref().set({
-    //  unloaded: "yes"
-    //})
+      if (player1Choice === "paper" && player2Choice === "rock") {
+          player1Wins++
+          player2Losses++
+      }
+      if (player1Choice === "paper" && player2Choice === "paper") {
+          ties++
+      }
+      if (player1Choice === "paper" && player2Choice === "scissors") {
+          player2Wins++
+          player1Losses++
+      }
+
+      if (player1Choice === "scissors" && player2Choice === "rock") {
+          player2Wins++
+          player1Losses++
+      }
+      if (player1Choice === "scissors" && player2Choice === "paper") {
+          player1Wins++
+          player2Losses++
+      }
+      if (player1Choice === "scissors" && player2Choice === "scissors") {
+          ties++
+      }
+
+      db.ref().update({
+          player1Choice: "!",
+          player2Choice: "!"
+      })
+
+      db.ref("gameData/scores").update({
+          player1Wins: player1Wins,
+          player1Losses: player1Losses,
+          player2Wins: player2Wins,
+          player2Losses: player2Losses,
+          ties: ties
+      })
+
+      $("#p1Wins").text(player1Wins)
+      $("#p1Losses").text(player1Losses)
+      $("#p2Wins").text(player2Wins)
+      $("#p2Losses").text(player2Losses)
+      $("#ties").text(ties)
+      $("#p1Choice").text("Waiting")
+      $("#p2Choice").text("Waiting")
+
+
+  }
+
+  function resetGame() {
+
+  }
+
+  db.ref().on("value", function (snapshot) {
+      if (snapshot.exists()) { // if player1 choice exists...
+          if (snapshot.val().player1Choice !== undefined && snapshot.val().player2Choice !== undefined) {
+              if (snapshot.val().player1Choice !== "!" && snapshot.val().player2Choice !== "!") {
+                  alert("both players have chosen")
+                  endGame(snapshot.val().player1Choice, snapshot.val().player2Choice)
+              }
+          }
+
+          if (snapshot.val().player1Choice !== undefined && snapshot.val().player1Choice !== "!"){
+              $("#p1Choice").text("Chosen")
+          } else {
+              $("#p1Choice").text("Waiting...")
+          }
+          if (snapshot.val().player2Choice !== undefined && snapshot.val().player2Choice !== "!"){
+              $("#p2Choice").text("Chosen")
+          } else {
+              $("#p2Choice").text("Waiting...")
+          }
+      } else {
+          alert("player 1 choice doesnt exist")
+      }
   })
 
-  $("#name-div").prop("disabled", true)
 })
